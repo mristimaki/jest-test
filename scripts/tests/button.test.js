df0586294c01c265948c3469e44288478af93b54
mristimaki <rosanna.ristimaki@gmail.com>
2023-06-06 03:25:17 +0000

    Add test to test the DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,13 +1,23 @@
 const { TestScheduler } = require("jest");
 const buttonClick = require("../button");
 
+// the node fs module is a file system module 
+// built in to node, that allows us to open, 
+// read and write files.
 beforeEach( () => {
-    document.body.innerHTML = "<p id='par'></p>"
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
